50921921c1366c7e13b256f96e28855cd4e088ba
Federico Lencina <federiconahuellencina@gmail.com>
2022-08-14 19:14:37 -0300

    Added images and a logo to the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,14 +5,18 @@ import "./Navbar.css";
 
 function NavBar() {
     return (
-      <Navbar bg="light" expand="lg">
+      <Navbar expand="lg" className="navBar">
         <Container className="nav">
-          <Navbar.Brand href="#home" className="w-50">
-            Drizzle Indumentaria
+          <Navbar.Brand href="#home" className="text-center logoContainer">
+            <img
+              alt=""
+              src={require("../../images/logo/logo1.png")}
+              className="d-inline-block align-top logo"
+            />{" "}
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto justify-content-around w-100">
+          <Navbar.Collapse id="basic-navbar-nav navLinksContainer">
+            <Nav className="me-auto justify-content-bewtween navBarLinks">
               <Nav.Link href="#home" className="navLink">
                 Home
               </Nav.Link>
