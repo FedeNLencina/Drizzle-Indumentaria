a86dd372589c52d4ea778f7d4c7aac7e98219afb
Federico Lencina <federiconahuellencina@gmail.com>
2022-08-25 11:40:24 -0300

    Added item counter

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -3,18 +3,41 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import "./ItemCount.css";
 
-export function ItemCount(props) {
+export function ItemCount({ stock, initial, agregarProducto }) {
+  const [counter, setCounter] = useState(initial);
 
+  const increase = () => {
+    if (counter < stock) {
+      setCounter(counter + 1);
+    }
+  };
+
+  const dicrease = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+  
   return (
     <Container className="d-flex justify-content-center">
-      <div className="itemCount d-flex justify-content-around">
-        <Button variant="outline-primary">-</Button>
-        <div className="d-flex flex-column justify-content-center">
-          <div> {props.cantidad}</div>
-          <div> 2</div>
-        </div>
+      <div className="itemCount d-flex flex-column justify-content-center">
+        <div className="d-flex justify-content-around">
+          <Button variant="outline-primary" onClick={dicrease}>
+            -
+          </Button>
+          <div className="d-flex flex-column justify-content-center">
+            <div>{counter}</div>
+          </div>
 
-        <Button variant="outline-primary">+</Button>
+          <Button variant="outline-primary" onClick={increase}>
+            +
+          </Button>
+        </div>
+        <div className="d-flex justify-content-center">
+          <Button variant="outline-primary" onClick={()=>agregarProducto(counter)}>
+            Agregar al carrito
+          </Button>
+        </div>
       </div>
     </Container>
   );
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,9 +2,14 @@ import Container from "react-bootstrap/Container";
 import { ItemCount } from "../ItemCount/ItemCount";
 
 export function ItemListContainer(props) {
+
+    const agregar = (counter) => {
+        console.log("agregado, ", counter);
+    }
+
     return (
         <Container className="text-center">
-            <ItemCount cantidad="Cantidad"/>
+            <ItemCount stock={10} initial={1} agregarProducto={agregar}/>
         </Container>
     )
 }
