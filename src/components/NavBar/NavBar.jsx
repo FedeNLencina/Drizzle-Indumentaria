6d108458ca622a9445419416ecfa379a00a90ace
Federico Lencina <federiconahuellencina@gmail.com>
2022-12-06 23:16:42 -0300

    Some adjustments to the navbar and begin responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 export function NavBar() {
   return (
     <Navbar expand="lg" className="navBar">
-      <Container className="nav">
+      <Container className="nav d-flex justify-content-center">
         <div className="d-flex justify-content-center logoContainer">
           <Link to="/" className="text-center logoLink">
             <img
@@ -18,9 +18,10 @@ export function NavBar() {
             />{" "}
           </Link>
         </div>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav navLinksContainer">
-          <Nav className="me-auto navBarLinks justify-content-around">
+        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+        {/* <Navbar.Collapse id="basic-navbar-nav navLinksContainer"> */}
+        <Nav className="me-auto d-flex navBarLinks justify-content-around">
+          <div className="divLinks d-flex justify-content-around">
             <Link to="/" className="navLink">
               Home
             </Link>
@@ -33,14 +34,18 @@ export function NavBar() {
             <Link to="/itemList/sweater" className="navLink">
               Sweaters
             </Link>
+            <Link to="/itemList/remera" className="navLink">
+              Remeras
+            </Link>
             <Link to="#link" className="navLink">
               Contact
             </Link>
-            <div className="widgetDiv d-flex justify-content-sm-start justify-content-lg-end">
-              <CarWidget />
-            </div>
-          </Nav>
-        </Navbar.Collapse>
+          </div>
+          <div className="widgetDiv d-flex justify-content-sm-start justify-content-lg-end">
+            <CarWidget />
+          </div>
+        </Nav>
+        {/* </Navbar.Collapse> */}
       </Container>
     </Navbar>
   );
