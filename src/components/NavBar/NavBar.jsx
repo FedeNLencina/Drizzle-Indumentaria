9f0756bcda31a8c3298e8439eda49b2d14084880
Federico Lencina <federiconahuellencina@gmail.com>
2022-12-09 14:21:39 -0300

    Some modifications on the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -20,56 +20,61 @@ export function NavBar() {
             />{" "}
           </Link>
         </div>
-        <Navbar.Toggle
-          aria-controls="responsive-navbar-nav"
-          className="colapsedButton"
-        />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto d-flex navBarLinks justify-content-around">
-            <Nav.Link href="#" className="d-flex justify-content-between">
-              <div className="d-flex justify-content-sm-start">
-                <Link to="/" className="navLink text-center">
-                  Home
+        
+          <Navbar.Toggle
+            aria-controls="responsive-navbar-nav"
+            className="colapsedButton"
+          />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto d-flex navBarLinks justify-content-around">
+              <Nav.Link
+                href="#"
+                className="d-flex justify-content-between navLinkHome"
+              >
+                <div className="d-flex justify-content-sm-start">
+                  <Link to="/" className="navLinkHomeA text-center">
+                    Home
+                  </Link>
+                </div>
+                <Link to="#" className="navWidgetResponsive">
+                  <div className="widgetDivResponsive d-flex justify-content-sm-center justify-content-lg-end">
+                    <CarWidget />
+                  </div>
                 </Link>
-              </div>
-              <Link to="#" className="navWidgetResponsive">
-                <div className="widgetDivResponsive d-flex justify-content-sm-center justify-content-lg-end">
+              </Nav.Link>
+              <Nav.Link href="#">
+                <Link to="/itemList/catalogo" className="navLink text-center">
+                  Catalogo
+                </Link>
+              </Nav.Link>
+              <Nav.Link href="#">
+                <Link to="/itemList/vestido" className="navLink text-center">
+                  Vestidos
+                </Link>
+              </Nav.Link>
+              <Nav.Link href="#">
+                <Link to="/itemList/sweater" className="navLink text-center">
+                  Sweaters
+                </Link>
+              </Nav.Link>
+              <Nav.Link href="#">
+                <Link to="/itemList/remera" className="navLink text-center">
+                  Remeras
+                </Link>
+              </Nav.Link>
+              <Nav.Link href="#">
+                <Link to="#link" className="navLink text-center">
+                  Contact
+                </Link>
+              </Nav.Link>
+              <Nav.Link href="#" className="navWidget">
+                <div className="widgetDiv d-flex justify-content-sm-center justify-content-lg-end">
                   <CarWidget />
                 </div>
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="#">
-              <Link to="/itemList/catalogo" className="navLink text-center">
-                Catalogo
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="#">
-              <Link to="/itemList/vestido" className="navLink text-center">
-                Vestidos
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="#">
-              <Link to="/itemList/sweater" className="navLink text-center">
-                Sweaters
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="#">
-              <Link to="/itemList/remera" className="navLink text-center">
-                Remeras
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="#">
-              <Link to="#link" className="navLink text-center">
-                Contact
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="#"  className="navWidget">
-              <div className="widgetDiv d-flex justify-content-sm-center justify-content-lg-end">
-                <CarWidget />
-              </div>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+      
       </Container>
     </Navbar>
   );
