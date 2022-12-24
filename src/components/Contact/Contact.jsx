08bf273eae8d3fc0979304792f389d1a936f958e
Federico Lencina <federiconahuellencina@gmail.com>
2022-12-24 11:10:42 -0300

    Started contact component. Installed react-3d icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,3 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
+import { Icon } from "react-3d-icons";
+import { twitter, github } from "react-3d-icons";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-export const Contact = () => {};
+export const Contact = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Icon
+            file={twitter}
+            color={"#1DA1F2"}
+            scale={10}
+            style={{ height: "100px", width: "100px" }}
+          />
+        </Col>
+        <Col>
+          <Icon
+            file={github}
+            color={"#1c1c1c"}
+            scale={10}
+            style={{ height: "150px", width: "150px" }}
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
